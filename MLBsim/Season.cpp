5c059bfe55d8c550b678444a3d2ffa1690474253
StevenJBurns <stevenjburns@msn.com>
2016-10-17 15:31:47 -0600

    Comments for baseline chance to win a game

--- MLBsim/Season.cpp
+++ MLBsim/Season.cpp
@@ -20,4 +20,7 @@ void Season::BuildSchedule()
 Team Season::PlayGame(Team visitor, Team home)
   {
   return home;
+
+  // Set up a baseline for the home team's chance to win
+  // Starts even at 0.50 (i.e.50%), plus 0.05 (5%) bonus for homefield advantage
   }
